8a2bb4a775406f161eb1ee7468e1471167aeb8f1
joshuaj21 <joshuajung360@gmail.com>
2024-10-30 21:21:05 +0900

    20241030(1)

--- main.py
+++ main.py
@@ -39,15 +39,18 @@ class Comment:
     update_time:datetime = datetime.now()
 
 # 샘플 데이터
-posts = [
-    Post(id=1, title="첫 번째 게시물", content="이것은 첫 번째 게시물입니다.", user_id=1),
-    Post(id=2, title="두 번째 게시물", content="이것은 두 번째 게시물입니다.", user_id=2),
-]
+posts = {
+    1:Post(id=1, title="첫 번째 게시물", content="이것은 첫 번째 게시물입니다.", user_id=1),
+    2:Post(id=2, title="두 번째 게시물", content="이것은 두 번째 게시물입니다.", user_id=2),
+}
 
 users = [
     User(id=1, account="test1", name="이탁균", pw="123"),
     User(id=2, account="test2", name="정우성", pw="456")
 ]
+
+post_num = len(posts)
+user_num = len(users)
 
 @app.route('/')
 def index():
@@ -68,14 +71,16 @@ def get_post_list():
 
 @app.route('/post/create', methods=['POST'])
 def create_post():
+    global post_num
     print(request)
     print(request.form)
     title = request.form['title']
     content = request.form['content']
     user_id = request.form['user_id']
-    post_id = len(posts)+1
+    post_num += 1
+    post_id = post_num
     new_post = Post(post_id, title, content, user_id)
-    posts.append(new_post)
+    posts[post_id] = new_post
     return redirect(url_for("index"))
 
 @app.route('/post/edit', methods=['POST'])
@@ -88,25 +93,13 @@ def edit_post():
     print(id, title,content)
     user_id = request.form['user_id']
     new_post = Post(int(id), title, content,user_id)
-    posts[int(id)-1] = new_post
+    posts[int(id)] = new_post
     return redirect(url_for("index"))
 
-@app.route('/post/edit', methods=['POST'])
-def edit_post():
-    print(request)
-    print(request.form)
-    title = request.form['title']
-    content = request.form['content']   
-    id = request.form['postId']
-    print(id, title,content)
-    user_id = request.form['user_id']
-    new_post = Post(int(id), title, content,user_id)
-    posts[int(id)-1] = new_post
-    return redirect(url_for("index"))
-
-@app.route('/post/delete/<int:post_id>', methods=['POST'])
+@app.route('/post/delete/<int:post_id>', methods=['GET'])
 def delete_post(post_id):
-    del posts
+    del posts[post_id]
+    return redirect(url_for("index"))
 
 
 
